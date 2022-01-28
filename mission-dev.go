52e6bf789a3ce0db5e86e6a0b1e3f63e392a3bfa
James Kokou GAGLO <freemanpolys@gmail.com>
2022-01-28 07:24:58 +0100

    Update aws account and region for production deployment

--- mission-dev.go
+++ mission-dev.go
@@ -47,15 +47,15 @@ func env() *awscdk.Environment {
 	// Account/Region-dependent features and context lookups will not work, but a
 	// single synthesized template can be deployed anywhere.
 	//---------------------------------------------------------------------------
-	return nil
+	//return nil
 
 	// Uncomment if you know exactly what account and region you want to deploy
 	// the stack to. This is the recommendation for production stacks.
 	//---------------------------------------------------------------------------
-	// return &awscdk.Environment{
-	//  Account: jsii.String("123456789012"),
-	//  Region:  jsii.String("us-east-1"),
-	// }
+	return &awscdk.Environment{
+		Account: jsii.String("617097471695"),
+		Region:  jsii.String("us-east-1"),
+	}
 
 	// Uncomment to specialize this stack for the AWS Account and Region that are
 	// implied by the current CLI configuration. This is recommended for dev
